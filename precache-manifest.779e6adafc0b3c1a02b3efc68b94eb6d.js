4ed4bb1ac71b97600ddc7dfcb76d0eef4cd7c6a0
vivek <viveksingh101994@gmail.com>
2019-09-26 22:28:11 +0530

    Updates

---
+++ precache-manifest.779e6adafc0b3c1a02b3efc68b94eb6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a9dd5281502668898205387094213c",
+    "url": "/monster-o-pedia/index.html"
+  },
+  {
+    "revision": "a0b616986c37e2b6ccb6",
+    "url": "/monster-o-pedia/static/css/main.625f462e.chunk.css"
+  },
+  {
+    "revision": "e2525c32c8c225d5a1d2",
+    "url": "/monster-o-pedia/static/js/2.e6b78215.chunk.js"
+  },
+  {
+    "revision": "a0b616986c37e2b6ccb6",
+    "url": "/monster-o-pedia/static/js/main.3778d731.chunk.js"
+  },
+  {
+    "revision": "efaf71fcd0e171f562f6",
+    "url": "/monster-o-pedia/static/js/runtime-main.6aafe4e8.js"
+  }
+]);
